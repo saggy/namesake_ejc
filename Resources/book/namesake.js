50bdc27ea1880d525d973defea9971c2e850bcea
Robert Dupuy <rdupuy@umpublishing.org>
2013-06-26 14:01:44 -0500

    closes #79

--- Resources/book/namesake.js
+++ Resources/book/namesake.js
@@ -85,26 +85,7 @@ for (var i=0, max=all.length; i < max; i++) {
 		}
 	}
 	
-	
-	//have no idea, but this doesn't work in desktop Safari, so moved the logic above
-	/*
-	var elClass = all[i].className;
-	if(typeof(elClass) !== 'undefined' && elClass != null){
-		switch(elClass){
-			case 'answer':
-				all[i].id = 'a'+i;
-				break;
-			case 'blankfill':
-				all[i].id = 'bf'+i;
-				var blanks = all[i].innerHTML.length;
-				all[i].style.minWidth = blanks + 'em';
-				all[i].innerHTML = '';
-				break;
-		}
-	}
-	*/
-	
-	
+
 }
 }
 
@@ -280,6 +261,38 @@ function add_note (e) {
 }
 
 function add_search_highlight(e) {
+	var searchTerm = e.searchWord;
+    var highlightStartTag = "<mark>";
+    var highlightEndTag = "</mark>";
+
+  var bodyText = document.body.innerHTML;
+  var newText = "";
+  var i = -1;
+  var lcSearchTerm = searchTerm.toLowerCase();
+  var lcBodyText = bodyText.toLowerCase();
+    
+  while (bodyText.length > 0) {
+    i = lcBodyText.indexOf(lcSearchTerm, i+1);
+    if (i < 0) {
+      newText += bodyText;
+      bodyText = "";
+    } else {
+      // skip anything inside an HTML tag
+      if (bodyText.lastIndexOf(">", i) >= bodyText.lastIndexOf("<", i)) {
+        // skip anything inside a <script> block
+        if (lcBodyText.lastIndexOf("/script>", i) >= lcBodyText.lastIndexOf("<script", i)) {
+          newText += bodyText.substring(0, i) + highlightStartTag + bodyText.substr(i, searchTerm.length) + highlightEndTag;
+          bodyText = bodyText.substr(i + searchTerm.length);
+          lcBodyText = bodyText.toLowerCase();
+          i = -1;
+        }
+      }
+    }
+  }
+	document.body.innerHTML = newText;
+}
+
+function add_search_highlight_old(e) {
 	var searchWord = e.searchWord;
 
 var firstLetter = searchWord.charAt(0);
